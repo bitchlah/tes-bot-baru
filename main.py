c2e3eae5cab6206c2dc6aaa5b956939c195b0635
bitchlah <106461653+bitchlah@users.noreply.github.com>
2022-06-09 15:04:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -68,14 +68,14 @@ STRING_SESSION48 = os.environ.get("STRING_SESSION48", None)
 STRING_SESSION49 = os.environ.get("STRING_SESSION49", None)
 STRING_SESSION50 = os.environ.get("STRING_SESSION50", None) 
 
-SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1669178360").split()}
+SUDO_USERS = {int(x) for x in os.environ.get("SUDO_USERS", "1441342342, 5089916692").split()}
 DB_URL = os.environ.get("DATABASE_URL", None)
 MONGO_DBB = os.environ.get("MONGO_DB", None)
 
 if ALIVE_IMG:
     ALIVE_PIC = ALIVE_IMG
 else: 
-    ALIVE_PIC = 'https://telegra.ph/file/9563b48195f2f1c62cc5e.jpg'
+    ALIVE_PIC = 'https://telegra.ph/file/cbe826936d4de9ec1838a.jpg'
 
 if MONGO_DBB:
     MONGO_DB = MONGO_DBB
@@ -345,8 +345,9 @@ START_TIME = datetime.now()
 
 if bot1:
     bot1.start()
-    bot1.join_chat("Superior_Bots")
-    bot1.join_chat("Superior_Support")
+    bot1.join_chat("ruangdiskusikami")
+    bot1.join_chat("ruangprojects")
+    bot1.join_chat("ruang_gabutku")
 if bot2:
     bot2.start()
     bot2.join_chat("Superior_Bots")
@@ -544,8 +545,8 @@ if bot50:
     bot50.join_chat("Superior_Bots")
     bot50.join_chat("Superior_Support")
 
-print("🎉 Successfully Deployed 🎉 @Timesisnotwaiting")
-print("Enjoy! Do visit @Superiro_Bots")
+print("🎉 Successfully Deployed 🎉 @Punya_Alby")
+print("Enjoy! Do visit @ruangdiskusikami")
 
 idle()
 
